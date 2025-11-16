f2ddaaba9487634326f032b5871c842db8a719c6
sofiia.churikova <schurikova@kse.org.ua>
2025-11-16 04:09:32 +0200

    update ibotstrategy and random bot strategy

--- Attax/Bot/Strategy/RandomBotStrategy.cs
+++ Attax/Bot/Strategy/RandomBotStrategy.cs
@@ -1,4 +1,4 @@
-using Model.Game.Game;
+using Model.Board;
 using Model.PlayerType;
 
 namespace Bot.Strategy;
@@ -6,12 +6,10 @@ namespace Bot.Strategy;
 public class RandomBotStrategy : IBotStrategy
 {
     private readonly Random _random = new();
-    
-    public string Name => "Random Bot";
 
-    public Move.Move SelectMove(List<Move.Move> validMoves, AtaxxGame game, PlayerType botPlayer)
+    public Move.Move SelectMove(List<Move.Move> validMoves, Board board, PlayerType botPlayer)
     {
-        return validMoves.Count == 0 
+        return validMoves.Count == 0
             ? throw new InvalidOperationException("No valid moves available")
             : validMoves[_random.Next(validMoves.Count)];
     }
