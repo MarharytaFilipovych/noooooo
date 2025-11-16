41f819b0ff4add605875f760d74634de61d42e3d
sofiia.churikova <schurikova@kse.org.ua>
2025-11-16 04:07:13 +0200

    update bot orchestrator

--- Attax/Bot/Orchestrator/BotOrchestrator.cs
+++ Attax/Bot/Orchestrator/BotOrchestrator.cs
@@ -6,17 +6,17 @@ namespace Bot.Orchestrator;
 
 public class BotOrchestrator(IBotStrategy strategy, BotOptions options) : IBotOrchestrator
 {
-    private readonly IBotStrategy _strategy = strategy ?? throw new ArgumentNullException(nameof(strategy));
-
-    public void MakeBotMove(AtaxxGameWithEvents game, PlayerType botPlayer)
+    public Move.Move? SuggestMove(AtaxxGame game, PlayerType botPlayer)
     {
         Thread.Sleep(options.ThinkingDelayMs);
 
         var validMoves = game.GetValidMoves(botPlayer);
-        
-        if (validMoves.Count == 0) return;
 
-        var selectedMove = _strategy.SelectMove(validMoves, game, botPlayer);
-        game.MakeMove(selectedMove.From, selectedMove.To);
+        if (validMoves.Count == 0)
+        {
+            return null;
+        }
+
+        return strategy.SelectMove(validMoves, game.Board, botPlayer);
     }
 }
